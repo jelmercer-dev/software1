45fc178e949004ad1d5bd83797710cf7a61d9c98
Denys Khliustin <jelmercer@gmail.com>
2026-08-26 11:48:25 +0300

    completed ex6 mod3

---
+++ module3/exercise6.py
@@ -0,0 +1,17 @@
+import random
+
+number1 = random.randint(0, 9)
+number2 = random.randint(0, 9)
+number3 = random.randint(0, 9)
+
+print (f"3-digit code: {number1}{number2}{number3}")
+
+number1 = random.randint(1, 6)
+number2 = random.randint(1, 6)
+number3 = random.randint(1, 6)
+number4 = random.randint(1, 6)
+
+print (f"4-digit code: {number1}{number2}{number3}{number4}")
+
+
+
--- readme.md
+++ readme.md
@@ -8,7 +8,7 @@ I completed exercise.
 
 ## Module 3
 
-I completed exercises 1, 2, 3, 4, 5.
+I completed exercises 1, 2, 3, 4, 5, 6.
 
 ## Module 4
 
