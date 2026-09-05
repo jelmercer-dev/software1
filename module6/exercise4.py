2e5e93c34857471b58c0319a0a2aff12287c53b4
Denys Khliustin <jelmercer@gmail.com>
2026-09-05 21:01:00 +0300

    completed ex4 mod6

---
+++ module6/exercise4.py
@@ -0,0 +1,10 @@
+city_names = []
+
+for n in range(5):
+	city_names.append(input("Enter the name of a city: "))
+
+print(" ")
+print("The cities you entered: ")
+
+for city in city_names:
+	print(city)
