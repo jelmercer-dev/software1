f813e7ee3a75200be1a8a18c701033f560e8cb82
Denys Khliustin <jelmercer@gmail.com>
2026-09-06 18:32:58 +0300

    completed ex4 mod7

---
+++ module7/exercise4.py
@@ -0,0 +1,12 @@
+def sum_of_list(number_list):
+
+    sum = 0
+
+    for number in number_list:
+        sum += number
+
+    return sum
+
+number_list = [1, 2, 3, 4, 5]
+result = sum_of_list(number_list)
+print(f"The sum of the numbers in the list is: {result}")
