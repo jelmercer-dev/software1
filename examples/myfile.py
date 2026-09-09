3268cda641c6f026c167e57c04965de16420ef5c
Denys Khliustin <jelmercer@gmail.com>
2026-09-09 10:32:18 +0300

    some changes

--- examples/myfile.py
+++ examples/myfile.py
@@ -1,18 +1,10 @@
-"""def greet(times):
-    for i in range(6):
-        times += 1
-        print("Hello" + str(times))
+class Dog:
+    def __init__(self, name, birth_year):
+        self.name = name
+        self.birth_year = birth_year
 
-greet(0)
-greet(100)"""
+dog = Dog("Bubbles", 2022)
+dog1 = Dog("Waxz", 2076)
 
-def add(a, b):
-    result = a + b
-    return result
-
-result = add(3, 1)
-print(result)
-
-
-fg= add(76, 67)
-print(fg)
+print(f"{dog.name:s} was born in {dog.birth_year:d}.")
+print(f"{dog1.name:s} was born in {dog1.birth_year:d}.")
