53938bdbf12e2457afdbfc68985402f354971f50
Denys Khliustin <jelmercer@gmail.com>
2026-09-11 19:43:54 +0300

    completed ex1 mod8 using tuple

--- module8/exercise1.py
+++ module8/exercise1.py
@@ -1,31 +1,30 @@
-def get_season (month):
-    season = month
-    while True:
-        if month == 1 or month == 2 or month == 12:
-            print("You entered: " + str(month))
-            print("The season is winter.")
-            break
+def get_season(month):
+    
+    winter = (1, 2, 12)
+    spring = (3, 4, 5)
+    summer = (6, 7, 8)
+    autumn = (9, 10, 11)
 
-        if month== 3 or month == 4 or month == 5:
-            print("You entered: " + str(month))
-            print("The season is spring.")
-            break
+    if month in winter:
+        season = "winter"
 
-        if month== 6 or month == 7 or month == 8:
-            print("You entered: " + str(month))
-            print("The season is summer.")
-            break
+    elif month in spring:
+        season = "spring"
 
-        if month== 9 or month == 10 or month == 11:
-            print("You entered: " + str(month))
-            print("The season is autumn.")
-            break
-        else:
-            print("You entered: " + str(month))
-            print("Please enter a number between 1 and 12.")
+    elif month in summer:
+        season = "summer"
 
-        return season
+    elif month in autumn:
+        season = "autumn"
+
+    else:
+        print("You entered: "+ str(month))
+        print("Please enter a number between 1 and 12.")
+        return
+
+    print("You entered:", month)
+    print("The season is", season + ".")
+
 
 month = int(input("Enter the number of a month (1-12): "))
-
-season = get_season(month)
+get_season(month)
